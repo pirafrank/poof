d6601c0c4b2c6f68234a3ef70b2010d61440f21a
Francesco Pira <dev@fpira.com>
2025-04-28 13:01:10 +0200

    feat: better dir structure in data dir

--- src/filesys.rs
+++ src/filesys.rs
@@ -6,6 +6,10 @@ use std::{
 
 use poof::SUPPORTED_EXTENSIONS;
 
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const DATA_SUBDIR: &str = "data";
+const BIN_SUBDIR: &str = "bin";
+
 // Constants for magic numbers
 #[cfg(target_os = "macos")]
 const MACHO_MAGIC_NUMBERS: &[[u8; 4]] = &[
@@ -17,51 +21,74 @@ const MACHO_MAGIC_NUMBERS: &[[u8; 4]] = &[
     [0xBE, 0xBA, 0xFE, 0xCA], // Mach-O universal ('fat') binary (big-endian)
 ];
 
-// ~/.config/APPNAME/config.json
+/// This function returns the path to the config directory for the application.
+/// It creates the directory if it doesn't exist.
+///
+/// Linux: ~/.config/APPNAME/config
+///
+/// macOS: ~/Library/Application Support/APPNAME/config
+///
+/// Windows: %APPDATA%/APPNAME/config
+///
 pub fn _get_config_dir() -> Option<PathBuf> {
-    if let Some(app_name) = option_env!("CARGO_PKG_NAME") {
-        let config_dir = dirs::config_dir()?.join(app_name);
-        if !config_dir.exists() {
-            std::fs::create_dir_all(&config_dir).ok()?;
-        }
-        Some(config_dir)
-    } else {
-        None
+    let config_dir = dirs::config_dir()?.join(APP_NAME).join("config");
+    if !config_dir.exists() {
+        std::fs::create_dir_all(&config_dir).ok()?;
     }
+    Some(config_dir)
 }
 
-// ~/.local/share/APPNAME
+/// This function returns the path to the data directory for the application.
+/// It creates the directory if it doesn't exist.
+///
+/// Linux: $HOME/.local/share/APPNAME/data
+///
+/// macOS: ~/Library/Application Support/APPNAME/data
+///
+/// Windows: %LOCALAPPDATA%/APPNAME/data
+///
 pub fn get_data_dir() -> Option<PathBuf> {
-    if let Some(app_name) = option_env!("CARGO_PKG_NAME") {
-        let data_dir = dirs::data_dir()?.join(app_name);
-        if !data_dir.exists() {
-            std::fs::create_dir_all(&data_dir).ok()?;
-        }
-        Some(data_dir)
-    } else {
-        None
+    let data_dir = dirs::data_dir()?.join(APP_NAME).join(DATA_SUBDIR);
+    if !data_dir.exists() {
+        std::fs::create_dir_all(&data_dir).ok()?;
     }
+    Some(data_dir)
 }
 
+/// This function returns the path to the bin directory for the application.
+/// It creates the directory if it doesn't exist.
+/// This is where the binaries will be stored.
+///
+/// Linux: ~/.local/share/APPNAME/bin
+///
+/// macOS: ~/Library/Application Support/APPNAME/bin
+///
+/// Windows: %LOCALAPPDATA%/APPNAME/bin
+///
 pub fn get_bin_dir() -> Option<PathBuf> {
-    let bin_dir = get_data_dir().unwrap().join("bin");
+    let bin_dir = dirs::data_dir()?.join(APP_NAME).join(BIN_SUBDIR);
     if !bin_dir.exists() {
         std::fs::create_dir_all(&bin_dir).ok()?;
     }
     Some(bin_dir)
 }
 
-// ~/.cache/APPNAME
+/// This function returns the path to the cache directory for the application.
+/// It creates the directory if it doesn't exist.
+/// This is where the cache files will be stored.
+///
+/// Linux: ~/.cache/APPNAME
+///
+/// macOS: ~/Library/Caches/APPNAME
+///
+/// Windows: %LOCALAPPDATA%/APPNAME/cache
+///
 pub fn get_cache_dir() -> Option<PathBuf> {
-    if let Some(app_name) = option_env!("CARGO_PKG_NAME") {
-        let cache_dir = dirs::cache_dir()?.join(app_name);
-        if !cache_dir.exists() {
-            std::fs::create_dir_all(&cache_dir).ok()?;
-        }
-        Some(cache_dir)
-    } else {
-        None
+    let cache_dir = dirs::cache_dir()?.join(APP_NAME);
+    if !cache_dir.exists() {
+        std::fs::create_dir_all(&cache_dir).ok()?;
     }
+    Some(cache_dir)
 }
 
 #[cfg(target_os = "linux")]
