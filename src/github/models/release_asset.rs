e40c282e728f27ad4bcff318ba0101a713bdb4de
Francesco Pira <dev@fpira.com>
2025-04-30 00:49:35 +0200

    refactor: code cleanup

--- src/github/models/release_asset.rs
+++ src/github/models/release_asset.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize, Debug)]
+/// Represents a release asset from GitHub.
 pub struct ReleaseAsset {
     name: String,
     content_type: String,
@@ -8,14 +9,6 @@ pub struct ReleaseAsset {
     browser_download_url: String,
 }
 impl ReleaseAsset {
-    pub fn new(name: String, content_type: String, browser_download_url: String) -> Self {
-        Self {
-            name,
-            content_type,
-            browser_download_url,
-        }
-    }
-
     pub fn name(&self) -> &String {
         &self.name
     }
@@ -26,15 +19,5 @@ impl ReleaseAsset {
 
     pub fn browser_download_url(&self) -> &String {
         &self.browser_download_url
-    }
-
-    pub fn set_name(&mut self, name: String) {
-        self.name = name;
-    }
-    pub fn set_content_type(&mut self, content_type: String) {
-        self.content_type = content_type;
-    }
-    pub fn set_browser_download_url(&mut self, browser_download_url: String) {
-        self.browser_download_url = browser_download_url;
     }
 }
