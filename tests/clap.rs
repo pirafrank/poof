8cd4f34c5cbb6242863b63323c4f266d6582153a
Francesco Pira <dev@fpira.com>
2025-05-24 16:45:27 +0000

    test: first work on integration tests

---
+++ tests/clap.rs
@@ -0,0 +1,14 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn test_command_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("poof")?;
+    cmd.arg("versioning")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("unrecognized subcommand"));
+    Ok(())
+}
+
