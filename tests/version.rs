0c5e555a88c44d1bccee2bdd37bf4f35a8030f9e
Francesco Pira <dev@fpira.com>
2025-05-28 12:44:46 +0000

    added info command test

--- tests/version.rs
+++ tests/version.rs
@@ -1,16 +1,20 @@
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+// Test for the 'version' subcommand
+#[cfg(test)]
+mod tests {
+    use assert_cmd::prelude::*; // Add methods on commands
+    use predicates::prelude::*; // Used for writing assertions
+    use std::process::Command; // Run programs
 
-#[test]
-fn test_command_exists() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("poof")?;
-    cmd.arg("version")
-        .assert()
-        .success()
-        .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")))
-        .stdout(predicate::str::contains("Commit"))
-        .stdout(predicate::str::contains("Build Date"))
-        .stderr(predicate::str::is_empty());
-    Ok(())
+    #[test]
+    fn test_command_exists() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = Command::cargo_bin("poof")?;
+        cmd.arg("version")
+            .assert()
+            .success()
+            .stdout(predicate::str::contains(env!("CARGO_PKG_VERSION")))
+            .stdout(predicate::str::contains("Commit"))
+            .stdout(predicate::str::contains("Build Date"))
+            .stderr(predicate::str::is_empty());
+        Ok(())
+    }
 }
