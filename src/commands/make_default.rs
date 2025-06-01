1913a9d61729e25f4757372910ec5aa82de6f78a
Francesco Pira <dev@fpira.com>
2025-06-01 16:49:10 +0000

    feat: better handling of error case for 'use' subcommand

--- src/commands/make_default.rs
+++ src/commands/make_default.rs
@@ -5,16 +5,32 @@ use log::error;
 use crate::files::datadirs;
 use crate::files::filesys;
 
-pub fn set_default(repo: &str, version: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn get_installed_dir(repo: &str, version: &str) -> Result<PathBuf, Box<dyn std::error::Error>> {
     let data_dir: PathBuf = datadirs::get_data_dir().ok_or(libc::ENOENT).unwrap();
-    let install_dir: PathBuf = datadirs::get_binary_nest(&data_dir, repo, version);
-    if !install_dir.exists() {
+    let installed_repo_dir = datadirs::get_versions_nest(&data_dir, repo);
+    if !installed_repo_dir.exists() {
         error!(
-            "Version {} of repository '{}' not installed. Quitting.",
+            "Repository '{}' is not installed. Quitting.",
+            repo
+        );
+        std::process::exit(110);
+    }
+
+    let installed_version_dir: PathBuf = datadirs::get_binary_nest(&data_dir, repo, version);
+    if !installed_version_dir.exists() {
+        error!(
+            "Version {} of repository '{}' is not installed. Quitting.",
             version, repo
         );
         std::process::exit(110);
     }
+
+    return Ok(installed_version_dir);
+}
+
+pub fn set_default(repo: &str, version: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // Get the installed directory for the specified repo and version
+    let install_dir: PathBuf = get_installed_dir(repo, version)?;
     // Get the bin directory
     let bin_dir: PathBuf = datadirs::get_bin_dir().ok_or(libc::ENOENT).unwrap();
     // Process each binary in wanted_dir
