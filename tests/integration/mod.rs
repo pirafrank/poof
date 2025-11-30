46e1d2ca683cc0517e5aae83f883043744de6ac4
Francesco Pira <dev@fpira.com>
2025-11-30 19:12:18 +0100

    delete ununsed files

--- tests/integration/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Integration tests for stateful commands
-
-mod list;
-mod make_default;
-mod enable;
-mod error_handling;
-mod download;
-mod install;
-mod update;
--- tests/unit/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Unit tests for standalone commands
-
-mod version;
-mod info;
-mod check;
-mod error_handling;
-mod command_handling;
-mod help;
