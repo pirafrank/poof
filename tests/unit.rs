60ba6dbe2ddd38f40560b336c49846815d4ee17d
Francesco Pira <dev@fpira.com>
2025-12-01 20:19:44 +0100

    test: new tests for archive detection, extraction, and validation

--- tests/unit.rs
+++ tests/unit.rs
@@ -4,9 +4,6 @@
 #[path = "unit/github_client.rs"]
 mod github_client;
 
-#[path = "unit/files/archives.rs"]
-mod archives;
-
 #[path = "unit/files/utils/find_similar_repos.rs"]
 mod find_similar_repos;
 
@@ -21,3 +18,12 @@ mod utils_get_stem_name_trimmed_at_first_separator;
 
 #[path = "unit/files/utils/strip_supported_extensions.rs"]
 mod utils_strip_supported_extensions;
+
+#[path = "unit/files/archives/validate_magic_bytes.rs"]
+mod archives_validate_magic_bytes;
+
+#[path = "unit/files/archives/common.rs"]
+mod archives_common;
+
+#[path = "unit/files/archives/extract_to_dir.rs"]
+mod archives_extract_to_dir;
