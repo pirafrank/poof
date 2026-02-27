5fe0a0318e1154a130d5926551ace1dd75134713
Francesco Pira <dev@fpira.com>
2026-02-27 22:07:11 +0100

    fix: support release assets with nested structure
    cli/cli is one of those assets with nested structure inside the archive

--- src/files/filesys.rs
+++ src/files/filesys.rs
@@ -1,6 +1,6 @@
 //! File system helpers for locating, copying, and symlinking executables.
 
-use log::{debug, warn};
+use log::{debug, info, warn};
 use std::path::{Path, PathBuf};
 
 use crate::files::magic::is_exec_by_magic_number;
@@ -12,18 +12,31 @@ use crate::files::utils::strip_supported_extensions;
 /// `true` for it. Directories and symlinks are ignored.
 pub fn find_exec_files_in_dir(dir: &Path) -> Vec<PathBuf> {
     let mut result: Vec<PathBuf> = Vec::new();
-    if let Ok(entries) = std::fs::read_dir(dir) {
+    let mut stack: Vec<PathBuf> = vec![dir.to_path_buf()];
+
+    while let Some(dir) = stack.pop() {
+        let entries = std::fs::read_dir(dir).unwrap();
         for entry in entries.flatten() {
+            // going entry.file_type avoid extra stat sys call
             if let Ok(file_type) = entry.file_type() {
+                // this works for small directories trees like the ones we have in
+                // the install directory or inside an extracted archive.
+                // if it's a directory, we add it to the stack to explore it recursively,
+                // otherwise we check if the file is a binary and add it to the result list.
                 // check criteria to determine if a file is a binary
                 // 1. Check if the file is a regular file
                 // 2. Check if the file is an executable by checking the magic number
-                if file_type.is_file() && is_exec_by_magic_number(&entry.path()) {
+                if file_type.is_dir() {
+                    stack.push(entry.path());
+                } else if file_type.is_file() && is_exec_by_magic_number(&entry.path()) {
                     result.push(entry.path());
-                }
+                    let s = entry.path().display().to_string();
+                    info!("Checking file: {}", s);
+                } // else we don't care
             }
         }
     }
+
     result
 }
 
