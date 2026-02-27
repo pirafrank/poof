8fcc0b8dad754a18c246ec03666d76b66df32d15
Francesco Pira <dev@fpira.com>
2026-02-27 10:02:07 +0100

    feat: apt and yum repo

---
+++ repo/src/index.js
@@ -0,0 +1,29 @@
+export default {
+    async fetch(request, env) {
+      if (request.method !== 'GET' && request.method !== 'HEAD') {
+        return new Response('Method Not Allowed', { status: 405 });
+      }
+
+      const url = new URL(request.url);
+      const key = url.pathname.slice(1); // strip leading /
+
+      if (!key) {
+        return new Response('Not Found', { status: 404 });
+      }
+
+      const object = await env.BUCKET.get(key);
+
+      if (!object) {
+        return new Response('Not Found', { status: 404 });
+      }
+
+      const headers = new Headers();
+      object.writeHttpMetadata(headers);
+      headers.set('etag', object.httpEtag);
+
+      return new Response(object.body, {
+        status: 200,
+        headers,
+      });
+    },
+  };
